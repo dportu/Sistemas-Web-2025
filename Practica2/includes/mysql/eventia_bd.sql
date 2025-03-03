424e9aabc1ad296f176fb679159a77b0a82bb8f4
Nayrasoy <naybetan@ucm.es>
2025-03-04 00:07:48 +0100

    Modificacion bd

--- Practica2/includes/mysql/eventia_bd.sql
+++ Practica2/includes/mysql/eventia_bd.sql
@@ -5,6 +5,12 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de datos: `eventia_db`
 --
@@ -31,45 +37,8 @@ CREATE TABLE `eventos` (
 
 INSERT INTO `eventos` (`id`, `nombre`, `descripcion`, `fecha_inicio`, `fecha_fin`, `lugar`, `organizador`) VALUES
 (1, 'Concierto de Metalica', NULL, '2025-03-15', NULL, NULL, NULL),
-(2, 'Concierto Anuel AA', NULL, '2025-03-31', NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `foro`
---
-
-CREATE TABLE `foro` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `autor` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `mensaje` text NOT NULL,
-  `evento` int(11) DEFAULT NULL,
-  `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `foro`
---
-
-INSERT INTO `foro` (`id`, `titulo`, `autor`, `email`, `mensaje`, `evento`, `fecha_publicacion`) VALUES
-(2, 'd', '', '', 'dd', 1, '2025-02-27 10:22:44'),
-(3, 'd', '', '', 'xc', 2, '2025-02-27 10:23:46');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(2, 'Concierto Anuel AA', NULL, '2025-03-31', NULL, NULL, NULL),
+(3, 'Halloween en Fabrik', 'Halloween en Fabrik!! No te lo pierdas', '2026-10-31', NULL, 'Fabrik', 'Eventia');
 
 --
 -- Índices para tablas volcadas
@@ -82,20 +51,6 @@ ALTER TABLE `eventos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `foro`
---
-ALTER TABLE `foro`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `evento` (`evento`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -104,28 +59,6 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `eventos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `foro`
---
-ALTER TABLE `foro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `foro`
---
-ALTER TABLE `foro`
-  ADD CONSTRAINT `foro_ibfk_1` FOREIGN KEY (`evento`) REFERENCES `eventos` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Practica2/vistas/comun/cabecera_foro.php
+++
@@ -1,5 +0,0 @@
-<header class="foro-header">
-    <div class="logo-eventia-box">
-        <a href="index.php">Eventia</a>
-    </div>
-</header>
