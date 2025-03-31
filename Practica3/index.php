df24d58ab1f62cc57950a74f1abb573a70b010cf
Nayrasoy <naybetan@ucm.es>
2025-03-31 17:34:10 +0200

    Creacion de clase valoracion

--- Practica3/index.php
+++ Practica3/index.php
@@ -7,6 +7,16 @@
 	$contenidoPrincipal = '';
 
 	// TODO: Mostrar los eventos que hay disponibles
+	
+	while ($evento = $eventos->fetch_array(MYSQLI_ASSOC)) {
+		$contenidoPrincipal .= '<div class="card mb-3">';
+		$contenidoPrincipal .= '<div class="card-body">';
+		$contenidoPrincipal .= '<h5 class="card-title">'.$evento['nombre'].'</h5>';
+		$contenidoPrincipal .= '<p class="card-text">'.$evento['descripcion'].'</p>';
+		$contenidoPrincipal .= '<p class="card-text"><small class="text-muted">Fecha: '.$evento['fecha'].'</small></p>';
+		$contenidoPrincipal .= '</div>';
+		$contenidoPrincipal .= '</div>';
+	}
 
 	require __DIR__.'/includes/vistas/plantillas/plantilla.php';
   
