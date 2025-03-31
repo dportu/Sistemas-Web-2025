6d6374c8727bf902382aa44584a4344cd3e27f4f
dportu <126913208+dportu@users.noreply.github.com>
2025-03-31 21:55:22 +0300

    inicio index y vistaEvento

--- Practica3/index.php
+++ Practica3/index.php
@@ -1,30 +1,32 @@
 <?php 
-
-	require_once __DIR__.'/includes/config.php';
 	use es\ucm\fdi\aw\eventos\Evento;
-
+	require_once __DIR__.'/includes/config.php';
+    
 	$tituloPagina = 'Inicio';
 
 	$contenidoPrincipal = '';
+	mostrarEventos($contenidoPrincipal);
 
-	// TODO: Mostrar los eventos que hay disponibles
-	$eventos = Evento::getEventos();
-	for ($i = 0; $i < count($eventos); $i++) {
-		$id = $eventos[$i]->id;
-		$imagen = $eventos[$i]->imagen;
-		$nombre = $eventos[$i]->nombre;
-		$precio = $eventos[$i]->precio;
-		$fecha = $eventos[$i]->fecha;
+	function mostrarEventos(&$contenidoPrincipal) {
+        $eventos = Evento::getEventos();
+            for ($i = 0; $i < count($eventos); $i++) {
+                $id = $eventos[$i]->id;
+                $imagen = $eventos[$i]->imagen;
+                $nombre = $eventos[$i]->nombre;
+                $precio = $eventos[$i]->precio;
+                $fecha = $eventos[$i]->fecha;
 
-		$contenidoPrincipal .= <<<EOS
-			<a href="vistaEvento.php?id={$id}" class="evento-card">
-				<img src="{$imagen}" alt="Imagen de {$nombre}" class="evento-imagen">
-				<h3>[ {$nombre} ]</h3>
-				<p> {$precio} € </p>
-				<p> {$fecha} </p>
-			</a>
-		EOS;
-	}
+                $contenidoPrincipal .= <<<EOS
+                    <a href="vistaEvento.php?id={$id}" class="evento-card">
+                        <img src="{$imagen}" alt="Imagen de {$nombre}" class="evento-imagen">
+                        <h3>[ {$nombre} ]</h3>
+                        <p> {$precio} € </p>
+                        <p> {$fecha} </p>
+                    </a>
+                EOS;
+            }
+        
+    }
 	
 	require __DIR__.'/includes/vistas/plantillas/plantilla.php';
   
