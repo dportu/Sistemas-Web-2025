e127adaf92537f17c8c48ad7ad389468d19a5837
alba.zz <152018226+albabast@users.noreply.github.com>
2025-04-03 18:48:15 +0200

    Add files via upload

--- Practica3/perfil.php
+++ Practica3/perfil.php
@@ -51,9 +51,8 @@
 <body>
     <div id="contenedor">
 
-    <?php require("includes/vistas/comun/cabecera.php"); ?>
+    <?php require("includes/vistas/comun/header.php"); ?>
 
-    <?php require("includes/vistas/comun/sidebarIzq.php"); ?>
     <main>
     <h1>Bienvenido, <?php echo htmlspecialchars($usuario_nombre); ?>!</h1>
     <p>Email: <?php echo htmlspecialchars($usuario_email); ?></p>
@@ -76,8 +75,8 @@
     
     <a href="logout.php">Cerrar sesión</a>
     </main>
-    <?php require("includes/vistas/comun/sidebarDer.php"); ?>
-    <?php require("includes/vistas/comun/pie.php"); ?>
+    <?php require("includes/vistas/comun/sidebar.php"); ?>
+    <?php require("includes/vistas/comun/footer.php"); ?>
     
     </div>
 </body>
