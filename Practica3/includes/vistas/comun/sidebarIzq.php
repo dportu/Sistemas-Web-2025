cada8af8795fedb2fb8e3c0c784d781d56c57ec0
Nayrasoy <naybetan@ucm.es>
2025-04-03 23:03:34 +0200

    Adios ficheros duplicados

--- Practica3/includes/vistas/comun/sidebarIzq.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <h2>Navegacion</h2>
-        <ul>
-            <li><a href= "index.php">Inicio</a></li>
-            <li><a href= "foro.php">Foro</a></li>
-            <li><a href= "admin.php">Administrador</a></li>
-        </ul>
-</nav>
