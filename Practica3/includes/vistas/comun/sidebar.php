a240a768afbe33a95c7cd5dc13daa64cf3e592ee
parlandis <117260873+parlandis@users.noreply.github.com>
2025-04-04 12:15:12 +0200

    side bar solo con foro y perfil

--- Practica3/includes/vistas/comun/sidebar.php
+++ Practica3/includes/vistas/comun/sidebar.php
@@ -2,6 +2,6 @@
     <h2>Navegacion</h2>
         <ul>
             <li><a href= "foro.php">Foro</a></li>
-            <li><a href= "admin.php">Administrador</a></li>
+            <li><a href= "perfil.php">Perfil</a></li>
         </ul>
 </nav>
